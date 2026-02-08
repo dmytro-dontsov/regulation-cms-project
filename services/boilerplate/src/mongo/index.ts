a2b149da48018a1a741df8b3ca3ec8d0b2b54679
dmytro-dontsov <qemmnes@gmail.com>
2026-02-08 11:26:51 +0100

    feat: setup mongodb connection with express and dotenv

---
+++ services/boilerplate/src/mongo/index.ts
@@ -0,0 +1,9 @@
+import { Router, Response } from 'express'
+import {getDB} from "../../../../common/mongo";
+
+export const router = Router();
+
+router.get('/mongo', async (_req, res: Response) => {
+    const collection = await getDB().collection('test');
+    res.json(await collection.find().toArray());
+});
---
+++ services/boilerplate/tests/mongo.spec.ts
@@ -0,0 +1,18 @@
+import request from 'supertest';
+import app from '../src/app';
+import {connectDB, getDB} from "../../../common/mongo";
+
+beforeAll(async () => {
+    await connectDB();
+})
+beforeEach(async () => {
+    await getDB().collection('test').deleteMany({});
+})
+it('should return test data from mongo', async () => {
+    await mockTestData();
+    const response = await request(app).get('/mongo').expect(200);
+    expect(response.body).toEqual([{_id: 1, name: 'test'}]);
+});
+const mockTestData = async () => {
+    await getDB().collection<{_id: number; name: string}>('test').insertOne({_id: 1, name: 'test'});
+}
